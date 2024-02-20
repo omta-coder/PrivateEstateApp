7a35a297b6f1f5116faebd5bab1549b973567078
omta <omtadayma2001@gmail.com>
2024-02-20 13:24:29 +0530

    complete update listing functionality

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createListing, deleteListing, updateListing } from "../controllers/listingController.js";
+import { createListing, deleteListing, getListing, updateListing } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create",verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
-router.post('/update/:id',verifyToken,updateListing)
+router.post('/update/:id',verifyToken,updateListing);
+router.get('/get/:id',getListing)
 
 export  default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -146,10 +146,10 @@ const Profile = () => {
     }
   };
 
-  const handleListingDelete = async (listingId) =>{
+  const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -157,12 +157,12 @@ const Profile = () => {
         return;
       }
       setUserListings((prev) =>
-      prev.filter((listing) => listing._id !== listingId)
-    );
+        prev.filter((listing) => listing._id !== listingId)
+      );
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -262,14 +262,28 @@ const Profile = () => {
               className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
               <Link to={`/listing/${listing._id}`}>
-                <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain' />
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
               </Link>
-              <Link to={`/listing/${listing._id}`} className='text-slate-700 font-semibold  hover:underline truncate flex-1'>
-              <p>{listing.name}</p>
+              <Link
+                to={`/listing/${listing._id}`}
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+              >
+                <p>{listing.name}</p>
               </Link>
-              <div className='flex flex-col item-center'>
-                <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
-                <button className='text-green-700 uppercase'>Edit</button>
+              <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
