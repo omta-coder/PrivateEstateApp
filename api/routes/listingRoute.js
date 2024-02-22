93e57ab800fdf21203a570e08052963266c379d9
omta <omtadayma2001@gmail.com>
2024-02-22 09:30:07 +0530

    create search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listingController.js";
+import { createListing, deleteListing, getListing, getListings, updateListing } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/create",verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing);
 router.post('/update/:id',verifyToken,updateListing);
-router.get('/get/:id',getListing)
+router.get('/get/:id',getListing);
+router.get('get',getListings);
 
 export  default router;
